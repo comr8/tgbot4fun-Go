53c0f58abd52960d56f6b3bfbb32c19e0671b741
c0mr8 <rhgftyurkeyutghjd@gmail.com>
2023-06-30 01:01:17 +0500

    Todo for getfreegame func

--- fun/freeEpicGames.go
+++ fun/freeEpicGames.go
@@ -31,3 +31,22 @@ func GetFreeGames() string {
 	resp = "Бесплатная игра в EpicGames: " + resp
 	return resp
 }
+
+//TODO Переделать метод с апи rapidapi, на метод о эпиков. Работает просто GET, без авторизации
+/* curl 'https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions?locale=en-US&country=RU&allowCountries=RU' \
+-H 'authority: store-site-backend-static-ipv4.ak.epicgames.com' \
+-H 'accept: application/json, text/plain, *\/*' \
+-H 'accept-language: ru-RU,ru;q=0.7' \
+-H 'origin: https://store.epicgames.com' \
+-H 'referer: https://store.epicgames.com/' \
+-H 'sec-ch-ua: "Not.A/Brand";v="8", "Chromium";v="114", "Brave";v="114"' \
+-H 'sec-ch-ua-mobile: ?0' \
+-H 'sec-ch-ua-platform: "Windows"' \
+-H 'sec-fetch-dest: empty' \
+-H 'sec-fetch-mode: cors' \
+-H 'sec-fetch-site: same-site' \
+-H 'sec-gpc: 1' \
+-H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36' \
+-H 'x-requested-with: XMLHttpRequest' \
+--compressed
+*/
